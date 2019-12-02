1f5c93236964a5c0604c989b226f5581d2515a78
Thomas Weland <47208777+Shedhatch@users.noreply.github.com>
2019-12-02 01:43:57 +0000

    Ajout des séances 1 & 2

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+//Séance 1
 
+//Dependencies
+const Discord = require('discord.js');
+const client = new Discord.Client(); //ou 'self' ou 'bot'
+const prefix = '/';+
+
+//Login
+client.login("BOT TOKEN")
+console.log("Connecté !")
+
+
+//Séance 2
+
+//simple message
+client.on("message", message => {
+if(message.content === prefix + 'salut')
+message.channel.send("Salut !")
+
+} )
+
+//message encadré
+client.on("message", message => {
+  if(message.content === prefix + "info"){
+ message.channel.send({embed: {
+  color: 0x11c191, //rgb: https://cog-creators.github.io/discord-embed-sandbox/ 
+  author: {
+  },
+  title: "AIDE AUX COMMANDES",
+  url: "",
+  description: `Voici toutes les commandes que du bot.`,
+  fields: [
+    {
+      name: `${prefix}`,
+      value: "Voici le préfix"
+    },
+     {
+      name: `${prefix}salut`,
+      value: "Le bot vous salura"
+    },
+
+    
+  ],
+  timestamp: new Date(),
+  footer: {
+    text: "Run me"
+  }
+}
+});
+}
+});
+
+//Séance 3 (à venir !)
